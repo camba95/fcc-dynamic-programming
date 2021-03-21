19c6da9c631a3b5f9252002c537bbd5d832219d2
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-03-21 17:47:11 -0400

    feat: grid traveler with memo

--- gridTraveler.js
+++ gridTraveler.js
@@ -8,8 +8,23 @@ const gridTraveler = (m, n) => {
 // O(m + n) space complexity 
 // height: n + m
 
-console.info(gridTraveler(1, 1));
-console.info(gridTraveler(2, 3));
-console.info(gridTraveler(3, 2));
-console.info(gridTraveler(3, 3));
-console.info(gridTraveler(18, 18));
+// Memoization
+
+const gridTraveler2 = (m, n, memo = {}) => {
+  const key = `${m},${n}`;
+  if (key in memo) return memo[key];
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  memo[key] = gridTraveler2(m - 1, n, memo) + gridTraveler2(m, n - 1, memo);
+  return memo[key];
+};
+
+// O(m * n) time complexity 
+// O(m + n) space complexity 
+// height: n + m
+
+console.info(gridTraveler2(1, 1));
+console.info(gridTraveler2(2, 3));
+console.info(gridTraveler2(3, 2));
+console.info(gridTraveler2(3, 3));
+console.info(gridTraveler2(18, 18));
