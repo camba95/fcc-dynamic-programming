315859a8a188a79096f6d3e98e0314e2633e0949
Kevin Alvarez <kevin.alvarez@trueforce.com>
2021-03-21 16:33:08 -0400

    feat: fib with memoization

--- fib.js
+++ fib.js
@@ -3,7 +3,22 @@ const fib = (n) => {
   return fib(n - 1) + fib(n - 2);
 };
 
-console.info(fib(6));
-console.info(fib(7));
-console.info(fib(8));
-console.info(fib(50));
+// O(2^n) time complexity
+// O(n) space complexity
+
+// Memoization
+
+const fib2 = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fib2(n - 1, memo) + fib2(n - 2, memo);
+  return memo[n];
+};
+
+// O(n) time complexity
+// O(n) space complexity
+
+console.info(fib2(6));
+console.info(fib2(7));
+console.info(fib2(8));
+console.info(fib2(50));
